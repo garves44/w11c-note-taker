a03f163acb467dc47cfbddf64a9c1d8c93403c5b
Garves <garves44@gmail.com>
2020-07-15 22:11:50 -0600

    Heroku debugging

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -106,7 +106,7 @@ var handleRenderSaveBtn = function() {
 // Render's the list of note titles
 var renderNoteList = function(notes) {
   $noteList.empty();
-
+  console.log(notes)
   var noteListItems = [];
   
   for (var i = 0; i < notes.length; i++) {
@@ -121,6 +121,7 @@ var renderNoteList = function(notes) {
     noteListItems.push($li);
   }
   $noteList.append(noteListItems);
+  
 };
 
 // Gets notes from the db and renders them to the sidebar
